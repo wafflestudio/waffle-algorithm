4a5c26f5aa8f32382af005b061a5c5bf3c69abda
Davin <bdv111@snu.ac.kr>
2021-03-15 00:39:55 +0900

    davin111 algorithm-interview 1,2

--- algorithm-interview/ch6/1/davin111_valid_palindrome.py
+++ algorithm-interview/ch6/1/davin111_valid_palindrome.py
@@ -1,2 +1,70 @@
-# davin111
-# valid palindrome
+"""
+Valid Palindrome
+author: davin111
+"""
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        """
+        avg runtime (5 attempts): 69.6ms
+        runtime beats: about 18%
+        memory usage beats: 65%
+        '풀이 4 - C 구현'과 유사한 부분 가짐
+        """
+        length = len(s)
+        marker = 0
+        for former in s:
+            if not former.isalnum():
+                continue
+            for j in range(marker, length):
+                latter = s[-1 - j]
+                if latter.isalnum():
+                    if former.lower() == latter.lower():
+                        marker = j + 1
+                        break
+                    else:
+                        return False
+                marker = j + 1
+        return True
+
+    def is_palindrome_deque(self, s: str) -> bool:
+        """
+        풀이 2 - 데크 자료형을 이용한 최적화
+        runtime: 44ms
+        """
+        import collections
+        from typing import Deque
+
+        strs: Deque = collections.deque()
+
+        for char in s:
+            if char.isalnum():
+                strs.append(char.lower())
+
+        while len(strs) > 1:
+            if strs.popleft() != strs.pop():
+                return False
+
+        return True
+
+    def is_palindrome_regex(self, s: str) -> bool:
+        """
+        풀이 3 - 슬라이싱 사용
+        runtime: 36ms
+        """
+        import re
+
+        s = s.lower()
+        s = re.sub('[^a-z0-9]', '', s)
+        return s == s[::-1]
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(str(solution.isPalindrome(input())).lower())
+
+"""
+wrong test cases on first submission
+",,,,,,,,,,,,acva" - alnum 조건이 있는 palindrome이므로, 단순히 중간 기준 X
+"""
--- algorithm-interview/ch6/2/davin111_reverse_string.py
+++ algorithm-interview/ch6/2/davin111_reverse_string.py
@@ -1,2 +1,57 @@
-# davin111
-# reverse string
+"""
+Reverse String
+author: davin111
+"""
+from typing import List
+
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        avg runtime (5 attempts): 188.8ms
+        runtime beats: about 91%
+        memory usage beats: 81%
+        '풀이 2 - 파이썬다운 방식'과 동일
+        """
+        s.reverse()
+
+    def reverse_string_bubble(self, s: List[str]) -> None:
+        """
+        avg runtime: Time Limit Exceeded
+        bubble sort와 유사
+        """
+        list_len = len(s)
+        for i in range(list_len - 1):
+            for j in range(list_len - 1 - i):
+                tmp = s[j + 1]
+                s[j + 1] = s[j]
+                s[j] = tmp
+                # s[j], s[j + 1] = s[j + 1], s[j] 도 가능
+
+    def reverse_string_two_pointers(self, s: List[str]) -> None:
+        """
+        풀이 1 - 투 포인터를 이용한 스왑
+        runtime: 196ms
+        """
+        left, right = 0, len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+
+
+if __name__ == "__main__":
+    import json
+
+    try:
+        s = json.loads(input())
+    except json.JSONDecodeError:
+        exit(1)
+
+    if not isinstance(s, list):
+        exit(1)
+
+    solution = Solution()
+    solution.reverseString(s)
+    print(s)
