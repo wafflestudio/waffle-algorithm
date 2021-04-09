e78f6e2773f7c8b480520162616c79e4856aa7dc
GoGiants1 <chooi221@snu.ac.kr>
2021-04-10 04:51:18 +0900

    gogiants1 p31 수정

--- algorithm-interview/ch11/31/gogiants1_top_K_frequent_elements.py
+++ algorithm-interview/ch11/31/gogiants1_top_K_frequent_elements.py
@@ -9,7 +9,7 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         result =[]
         cnt = collections.Counter(nums) # |nums| =: n 일떄,  O(n)
-        sorted_count = sorted(cnt.items(), key=lambda x: (-x[1], x[0])) # 정렬(팀 소트) -> O(nlogn)
+        sorted_count = sorted(cnt.items(), key=lambda x: (-x[1], x[0])) # 정렬(팀 소트) -> O( (n/c)log(n/c)). 단, c는 상수.
         
         # O(k)
         for i, v in enumerate(sorted_count): 
